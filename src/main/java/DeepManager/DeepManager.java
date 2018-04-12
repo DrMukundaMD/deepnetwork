f00d5f79129cd145518a7af22efbb4f41a84a27e
DrZeroMD <mukunda1008@gmail.com>
2018-04-12 00:43:01 -0400

    Starting DeepManager

---
+++ src/main/java/DeepManager/DeepManager.java
@@ -0,0 +1,19 @@
+package DeepManager;
+
+import java.util.HashMap;
+
+public class DeepManager {
+    private static HashMap<String, DeepTorrentManager> map;
+
+    public DeepManager(boolean isServerFlag){
+        map = new HashMap<>();
+    }
+
+    public void startTorrent(){
+
+    }
+
+    public void getSomething(){
+
+    }
+}
