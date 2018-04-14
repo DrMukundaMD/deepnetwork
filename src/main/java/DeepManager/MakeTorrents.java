03c25f6cede66e2035e902757c6e0f4d2705db52
DrZeroMD <mukunda1008@gmail.com>
2018-04-14 00:08:27 -0400

    Refactored and restructured project.

---
+++ src/main/java/DeepManager/MakeTorrents.java
@@ -0,0 +1,31 @@
+package DeepManager;
+
+import DeepThread.DeepLogger;
+import DeepThread.TorrentFolder;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class MakeTorrents {
+
+    public static ArrayList<String> makeAllTorrents(){
+        //iterate through to_torrents and compile torrents
+        String filename;
+        ArrayList<String> torrents = new ArrayList<>();
+        try {
+            for (File file : TorrentFolder.getToTorrent().listFiles()) {
+                if (file.isFile()) {
+                    filename = file.getName();
+                    torrents.add(filename);
+                    if(TorrentFolder.isTorrent(filename))
+                        CreateTorrentFile.create(file);
+                }
+
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+            DeepLogger.log(e.getMessage());
+        }
+        return torrents;
+    }
+}
